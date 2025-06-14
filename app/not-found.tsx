d38d81d9b3675ac045a4393404c36c8389322d88
bread0527 <jacob.miller0527@gmail.com>
2025-06-14 21:44:15 +0800

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+   // app/not-found.tsx
+   export default function NotFound() {
+     return (
+       <div style={{ textAlign: 'center', marginTop: '100px' }}>
+         <h2>页面未找到</h2>
+         <p>你访问的页面不存在。</p>
+       </div>
+     );
+   }
